971607165495360f9779ca00528c6d4fba072a2c
MVMali <mohanmali3822@gmail.com>
2024-01-25 11:55:11 +0530

    Ruby style guide

---
+++ ruby_style_guide.rb
@@ -0,0 +1,37 @@
+#https://rubystyle.guide/    reference
+
+#Use two spaces per indentation level (aka soft tabs).
+# bad - four spaces
+def some_method
+    do_something
+end
+
+# good
+def some_method
+  do_something
+end
+
+#Limit lines to 80 characters.
+
+#End each file with a newline.
+#Don’t use ; to terminate statements and expressions.
+#Use one expression per line.
+#Avoid dot where not required for operator method calls.
+# bad
+num.+ 42
+
+# good
+num + 42
+
+#Use spaces around operators, after commas, colons and semicolons.
+#No spaces after (, [ or before ], ). Use spaces around { and before }.
+
+#Use empty lines between method definitions and also to break up methods into logical paragraphs internally.
+
+#Avoid comma after the last parameter in a method call, especially when the parameters are not on separate lines.
+
+#Use snake_case for symbols, methods and variables.
+#Do not separate numbers from letters on symbols, methods and variables.
+
+#Use CapitalCase for classes and modules. (Keep acronyms like HTTP, RFC, XML uppercase).
+
