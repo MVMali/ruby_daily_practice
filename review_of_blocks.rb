7fba270a249d4c653a6c03647850620e005e4ee3
MVMali <mohanmali3822@gmail.com>
2024-01-29 16:49:16 +0530

    Code blocks in ruby

--- review_of_blocks.rb
+++ review_of_blocks.rb
@@ -12,8 +12,8 @@
 # Methods capture the repeatable steps of a procedure.
 # A block captures the custom step of a procedure.
 
-[1, 2, 3].each { |value| p value }
-p [1, 2, 3].map { |value| value ** 2 }
+# [1, 2, 3].each { |value| p value }
+# p [1, 2, 3].map { |value| value ** 2 }
 
 
 def pattern
@@ -24,13 +24,13 @@ def pattern
   yield
 end
 
-pattern{" A block is simply a chunk of code, and yield allows us to inject that code at some place into a method."}
+# pattern{" A block is simply a chunk of code, and yield allows us to inject that code at some place into a method."}
 
-pattern do
-  "Ruby"
-  "block of code"
-  puts "Methods consist of a name, but with blocks we don't need to write a name, and always pass to a method call"
-end
+# pattern do
+#   "Ruby"
+#   "block of code"
+#   puts "Methods consist of a name, but with blocks we don't need to write a name, and always pass to a method call"
+# end
 
 def block_given_method
   print "This block_given check if the block is given or not depending on that it throws exeption\n"
@@ -39,4 +39,36 @@ def block_given_method
   end
 end
 
-block_given_method{puts "Heyy the block is given"}
+# block_given_method{puts "Heyy the block is given"}
+
+#block parameters passing
+
+def passing_block_params(name)
+  yield(name,"Nice gesture")
+end
+
+# passing_block_params("Ram"){|name,sent| puts "#{name} you have #{sent}"}
+
+#custom_each method
+def custom_each(elements)
+  i = 0
+  while i < elements.length
+    yield elements[i]
+    i += 1
+  end
+end
+
+# custom_each([4,5,6]){|item|puts "Revenue in crores is #{item}"}
+
+def custom_map(elements)
+  arr = []
+  i = 0
+  while i < elements.length
+      x = yield elements[i]
+      arr.push(x) # or arr << x
+      i += 1
+  end
+  return arr
+end
+
+# custom_map([1,2,3]){|number| number*3}
